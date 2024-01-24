0356f1633908c017da935921e6f2e4ac80fd4b1a
Naza <sharon.doris54@gmail.com>
2024-01-24 04:27:48 -0800

    created and added some file

---
+++ nador-blog/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+    
+
+  return (
+    <div className="App">
+    <Navbar />
+      <div className="content">
+      <h1>App component</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
