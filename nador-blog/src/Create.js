3b3d9513953460392a7bc96367adaa6868125a4f
Naza <sharon.doris54@gmail.com>
2024-03-13 05:53:10 -0700

    Added some inputs inthe create.js file and also styled them

--- nador-blog/src/Create.js
+++ nador-blog/src/Create.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle ] = useState('');
+    const [body, setBody ] = useState('');
+    const [author, setAuthor ] = useState('sharon');
 
     return (
         <div className="create">
-        <h2>
-        Add a New Blog
-        </h2>
+        <h2>Add a New Blog</h2>
+        <form>
+        <label>Blog title:</label>
+        <input
+        type="text"
+        required
+        value={ title }
+        onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea 
+        required
+        value={ body }
+        onChange={(e) => setBody(e.target.value)}>
+        </textarea>
+        <label>Blog author:</label>
+        <select 
+        value={ author } 
+        onChange={(e) => setAuthor(e.target.value)}
+        >
+        <option value="naDor">Sharon</option>
+        <option value="pretty">Pearl</option>
+        <option value="tina">Destiny</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{ title }</p>
+        <p>{ body }</p>
+        <p>{ author }</p>
+        </form>
         </div>
     );
 }
