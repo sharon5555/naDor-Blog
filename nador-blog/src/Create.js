477b12a881506ef65a721f9cb3cab154c4337ac6
Naza <sharon.doris54@gmail.com>
2024-03-13 06:36:51 -0700

    programmatic redirects

--- nador-blog/src/Create.js
+++ nador-blog/src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
     const [title, setTitle ] = useState('');
     const [body, setBody ] = useState('');
     const [author, setAuthor ] = useState('sharon');
     const [isPending, setIsPending ] = useState(false);
+    const history = useHistory();
 
     const handleSubmit =(e) => {
         e.preventDefault();
@@ -15,12 +17,14 @@ const Create = () => {
         fetch('http://localhost:8000/blogs', {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
-            body: JSON.strinify(blog)
+            body: JSON.stringify(blog)
         }).then (() => {
             console.log('new blog added')
             setIsPending(false);
+            
+            history.push('/');
+        });
 
-        })
     }
 
     return (
